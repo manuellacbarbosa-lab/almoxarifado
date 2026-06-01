2a98895438351578edf42a171e176a0a4c77c240
MANUELLA CRISTINA DA SILVA BARBOSA <res0098994@aluno.firjansenaisesi.com.br>
2026-06-01 16:29:14 -0300

    Adicionando os Resources do Filament

---
+++ almoxarifado-app/app/Models/Movimento.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Produto;
+
+class Movimento extends Model
+{
+    protected $fillable = [
+        'produto_id', 'quantidade', 'tipo'
+    ];
+
+    public function produto()
+    {
+        return $this_>belongsTo(Produto::class,'produto_id');
+    }
+    
+}
